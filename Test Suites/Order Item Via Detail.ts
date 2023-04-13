22b9def1c1be9c602d6ca5bd6393fcfae7b5b882
hardi-yansah <hardi.yansah97@gmail.com>
2023-04-13 15:41:24 +0700

    Add testsuites Order Item Via Detail

---
+++ Test Suites/Order Item Via Detail.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Order Item Via Detail</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b1b0f439-20a3-4b3b-ac09-935b7ceb5620</testSuiteGuid>
+   <testCaseLink>
+      <guid>9bdec1f2-ee07-46f9-8b56-c60ec1db82c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order Item Via Detail/Order Item Via Detail - Sauce Labs Backpack</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a0ea380-4fc9-4612-93fd-a8f5f26a243f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order Item Via Detail/Order Item Via Detail - Sauce Labs Bike Light</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>385f02fc-7d88-4362-8a1c-8a374fd4776e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order Item Via Detail/Order Item Via Detail - Sauce Labs Bolt T-Shirt</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ce490bc6-1633-4578-a19e-5cc9a79e8c70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order Item Via Detail/Order Item Via Detail - Sauce Labs Fleece Jacket</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>48831584-d4a8-4bf6-80e5-b315a46b4ebd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order Item Via Detail/Order Item Via Detail - Sauce Labs Onesie</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5be97fb1-03c1-45ef-8002-73414040d6de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order Item Via Detail/Order Item Via Detail - Test.allTheThings() T-Shirt (Red)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
